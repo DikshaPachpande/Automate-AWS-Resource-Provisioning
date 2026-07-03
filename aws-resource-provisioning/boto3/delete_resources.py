73f81ee88f09e6a8ba122f1c9a7c5cdeec3c8004
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:45:44 +0530

    Update delete_resources.py

--- aws-resource-provisioning/boto3/delete_resources.py
+++ aws-resource-provisioning/boto3/delete_resources.py
@@ -1,1 +1,21 @@
+import boto3
+from config.config import REGION
+from config.config import S3_BUCKET
 
+ec2 = boto3.resource("ec2", region_name=REGION)
+
+# Stop all instances
+
+for instance in ec2.instances.all():
+    print("Terminating:", instance.id)
+    instance.terminate()
+
+s3 = boto3.resource("s3")
+
+bucket = s3.Bucket(S3_BUCKET)
+
+bucket.objects.all().delete()
+
+bucket.delete()
+
+print("Resources Deleted")
