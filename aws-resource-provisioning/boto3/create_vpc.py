19dc881ce0e80804021aee9de4cc860c4fab8b2c
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:45:02 +0530

    Update create_vpc.py

--- aws-resource-provisioning/boto3/create_vpc.py
+++ aws-resource-provisioning/boto3/create_vpc.py
@@ -1,1 +1,13 @@
+import boto3
+from config.config import REGION, VPC_CIDR
 
+ec2 = boto3.client("ec2", region_name=REGION)
+
+response = ec2.create_vpc(
+    CidrBlock=VPC_CIDR
+)
+
+vpc_id = response["Vpc"]["VpcId"]
+
+print("VPC Created")
+print(vpc_id)
