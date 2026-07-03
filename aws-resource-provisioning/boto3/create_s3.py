0693ddfa556a9bf07723882116230fad758c7658
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:44:38 +0530

    Update create_s3.py

--- aws-resource-provisioning/boto3/create_s3.py
+++ aws-resource-provisioning/boto3/create_s3.py
@@ -1,1 +1,14 @@
+import boto3
+from config.config import REGION, S3_BUCKET
 
+s3 = boto3.client("s3", region_name=REGION)
+
+response = s3.create_bucket(
+    Bucket=S3_BUCKET,
+    CreateBucketConfiguration={
+        "LocationConstraint": REGION
+    }
+)
+
+print("Bucket Created Successfully")
+print(response)
