e5de7733b230a18899a995ea1f61338a9c77964a
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:45:23 +0530

    Update create_ec2.py

--- aws-resource-provisioning/boto3/create_ec2.py
+++ aws-resource-provisioning/boto3/create_ec2.py
@@ -1,1 +1,19 @@
+import boto3
+from config.config import REGION
+from config.config import AMI_ID
+from config.config import INSTANCE_TYPE
+from config.config import KEY_NAME
 
+ec2 = boto3.resource("ec2", region_name=REGION)
+
+instance = ec2.create_instances(
+    ImageId=AMI_ID,
+    MinCount=1,
+    MaxCount=1,
+    InstanceType=INSTANCE_TYPE,
+    KeyName=KEY_NAME
+)
+
+print("EC2 Instance Created")
+
+print(instance[0].id)
